3d68f95145f51a0aefed63c5af3685977a1622e3
Daniel Kim Hangon <hangon1234@gmail.com>
2024-04-13 22:31:11 +0900

    update source code for assignment5

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -6,6 +6,7 @@
 #include <netdb.h>
 #include <string.h>
 
+#define BUFLEN 128 
 int main(void)
 {
     /* socket server for assignment 5 part 1 */
@@ -24,11 +25,27 @@ int main(void)
     hints.ai_flags = AI_PASSIVE;
 
     /* use getaddrinfo to get addrinfo structure */
-    int ret = getaddrinfo(NULL, "1000", &hints, &addrinfo_ptr);
+    getaddrinfo(NULL, "9000", &hints, &addrinfo_ptr);
 
     /* use bind to assign address */
-    int bind_ret = bind(socket_fd, addrinfo_ptr->ai_addr, sizeof(struct sockaddr));
+    bind(socket_fd, addrinfo_ptr->ai_addr, sizeof(struct sockaddr));
 
+    /* listen for connections on a socket */
+    listen(socket_fd, 10);
+
+    /* accept a connection on a socket */
+    struct sockaddr * client_addr = NULL;
+    socklen_t client_num = sizeof(struct sockaddr);
+    int fd_rw = accept(socket_fd, client_addr, &client_num); 
+
+    /* buffer to store received data */
+    char buf[BUFLEN];
+
+    /* receive from socket */
+    int recv_len = recv(socket_fd, buf, BUFLEN, 0);
+    printf("received: %d\n", recv_len);
+
+    
     /* free allocated addrinfo structure */
     freeaddrinfo(addrinfo_ptr);
 
