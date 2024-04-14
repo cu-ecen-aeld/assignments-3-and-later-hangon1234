b016c0cdb521cd40e43a22913b793bfb07332a92
Daniel Kim Hangon <hangon1234@gmail.com>
2024-04-14 21:59:14 +0900

    intermediate commit, not yet finished

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,9 @@
 #define _POSIX_C_SOURCE 200112L
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -24,7 +27,7 @@ int main(void)
     memset(&hints, 0, sizeof(struct addrinfo));
 
     /* setup hints structure */
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
@@ -33,11 +36,12 @@ int main(void)
 
     /* Check retcode of getaddrinfo */
     if (ret != 0) {
+	printf("getaddrinfo() failed!\n");
         return(ret); // exit if ret != 0
     }
 
     /* Get socket_fd */
-    int socket_fd = socket(addrinfo_ptr->ai_family, addrinfo_ptr->ai_socktype, addrinfo_ptr->ai_flags);
+    int socket_fd = socket(addrinfo_ptr->ai_family, addrinfo_ptr->ai_socktype, addrinfo_ptr->ai_protocol);
 
     /* check return value of socket
      * it returns fd if success
@@ -45,7 +49,15 @@ int main(void)
      */
 
     if (socket_fd == -1) { // socket() returns error
+        printf("socket() failed! errno: %d\n", errno);
         return(socket_fd);
+    }
+
+    /* to fix bind() address already in use */
+    int yes = 1;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror("setsockopt error");
+	exit(1);
     }
 
     /* use bind to assign address */
@@ -53,49 +65,63 @@ int main(void)
     
     /* bind() also returns -1 on error. check error */
     if (ret == -1) {
+	printf("bind() failed!\n");
         return(ret);
     }
 
+    /* free allocated addrinfo structure */
+    freeaddrinfo(addrinfo_ptr);
+
     /* handling for incoming connection */
-    ret = listen(socket_fd, BACKLOG);
+    ret = listen(socket_fd, 10);
     
     /* listen() also returns -1 on error. check error */
     if (ret == -1) {
+	printf("listen() failed!\n");
         return(ret);
     }
 
-    /* this structure will store information about the incoming connection 
-     * to determine which host is calling from which port 
-     */
-    struct sockaddr_storage connection_info;
-
-    /* size of the struct sockaddr_storage
-     * accept() will not put more bytes than addr_size
-     * If fewer, accetp() will change the value of addr_size
-     */
-    socklen_t addr_size = sizeof(connection_info);
-
     /* accept a connection on a socket */
-    int accept_fd = accept(socket_fd, (struct sockaddr *)&connection_info, &addr_size); 
+    struct sockaddr * client_addr = NULL;
+    socklen_t client_num = sizeof(struct sockaddr);
+    int fd_accept = accept(socket_fd, client_addr, &client_num); 
 
     /* accept() also returns -1 on error. check error */
-    if (accept_fd == -1){
-        return(accept_fd);
+    if (ret == -1) {
+	printf("accept() failed!\n");
+        return(ret);
     }
 
-    /* print information about the incoming connection */
-    printf("Accept connection from %s\n", ((struct sockaddr_in*)&connection_info)->sin_addr);
-
-    /* buffer to store received data */
+    /* Allocate buffer for socket operation */
     char buf[BUFLEN];
 
-    /* receive from socket */
-    int recv_len = recv(socket_fd, buf, BUFLEN, 0);
-    printf("received: %d\n", recv_len);
+    /* receive over socket */
+    ret = recv(fd_accept, buf, BUFLEN, 0);
 
+    printf("buffer received: %s\n", buf);
+    /* recv() returns -1 on error; returns number of bytes actually read into the buffer */
+    /* check if error is occured */
+    if (ret == -1) {
+	printf("recv() failed!\n");
+        return(ret);
+    }
+
+    /* send back received message to the client */
+    buf[BUFLEN] = '\0';
+    int len = strlen(buf);
+    printf("len: %d\n", len);
+    printf("buffer: %s\n", buf);
+    printf("=====\n");
+    int bytes_sent = send(fd_accept, buf, len , 0);
     
-    /* free allocated addrinfo structure */
-    freeaddrinfo(addrinfo_ptr);
+    printf("bytes sent: %d\n", bytes_sent);
+
+
+    /* close file descriptor */
+    shutdown(fd_accept, 2);
+    shutdown(socket_fd, 2);
+    close(socket_fd);
+    close(fd_accept);
 
     /* return 0 to indicate success */
     return 0;
