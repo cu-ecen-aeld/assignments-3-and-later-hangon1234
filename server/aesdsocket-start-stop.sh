f6758f3c6d1217ec64d595daf3ad13ceab8e0f6e
Daniel, Kim Hangon <hangon1234@gmail.com>
2024-05-15 07:38:31 +0000

    provide argument after -- are passed unmodified to the program being started

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,7 +3,7 @@
 case "$1" in
     start)
         echo "starting the aesdsocket"
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -d        ;;
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d        ;;
     stop)
         echo "stopping the aesdsocket"
         start-stop-daemon -K -n aesdsocket
